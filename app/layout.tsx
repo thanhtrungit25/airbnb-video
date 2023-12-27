2e62cf545353e3291379b1fb2c1d4bbba31e0295
Trung DT <thanhtrung.it25@gmail.com>
2023-12-27 17:00:14 +0700

    Add search filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import RegisterModal from './components/modals/RegisterModal'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <SearchModal />
         <LoginModal />
         <RegisterModal />
         <RentModal />
